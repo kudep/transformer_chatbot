982ce3ef1d0aac24ee18152e943063311135565f
Kuznetsov Denis Pavlovich <kuznetsov.den.p@gmail.com>
2019-04-03 13:21:22 +0300

    wip (optim): change a sin to a cos in a triangle opt.

--- model/optim.py
+++ model/optim.py
@@ -154,4 +154,4 @@ class TriangleOpt(NoamOpt):
             step = self._step
         scale_lr = self.factor * (self.embeddings_size ** (-0.5) * min(step ** (-0.5), step * self.warmup ** (-1.5)))
         scale_lr /= 2.0
-        return scale_lr + scale_lr * math.sin(2 * math.pi * self._step / self.step_period)
+        return scale_lr + scale_lr * math.cos(2 * math.pi * self._step / self.step_period)
